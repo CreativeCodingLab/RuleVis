9f044cfc498ee0ce142dd2263d72f94e8dffd62b
Cassia Artanegara <cassiarta@gmail.com>
2019-01-24 19:30:45 -0800

    svg centered; uses extraneous div which I am fixing now; code to be cleaned

--- index.js
+++ index.js
@@ -7,11 +7,24 @@ d3.select("body")
     .text("Rule-based modeling for complex biological systems");
 
 // Set up the SVG attributes
-var w = 500;
-var h = 500;
+var svgW = 500;
+var svgH = 500;
 
-// TODO: CENTER THE DIV
-var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+var windowW = document.documentElement.clientWidth || document.documentElement.body.clientWidth;
+
+var windowH = document.documentElement.clientHeight || document.documentElement.body.clientHeight;
+
+console.log(windowW);
+
+// Create a div that holds 
+var svgDiv = d3.select("body").append("div")
+                .attr("id", "svgDiv")
+                .attr("width", svgW)
+                .attr("height", svgH);
+var svg = svgDiv.append("svg")
+                .attr("width", svgW)
+                .attr("height", svgH)
+                .attr("transform", "translate(" + (windowW/2 - svgW/2) + ", 0)");
 
 // Manual dataset - sanity checks & experimentation
 var dataset = [ 5, 10, 15, 20, 25 ];
@@ -31,7 +44,7 @@ circles
     .attr("cx", function(d, i) {
         return (i * 50) + 25;
     })
-    .attr("cy", h/2)
+    .attr("cy", svgH/2)
     .attr("r", function(d) {
         if (d == "agent") {
             return 10;
