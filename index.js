62728de404058f41e0a73639dc1820110bf8c623
Mahika Dubey <mahika@Mahika-Dubeys-MacBook-Pro-2.local>
2019-01-25 17:18:55 -0800

    added force directed node generation

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ body.append("h2").text("Rule-based modeling for complex biological systems");
 
 // Set up the SVG attributes
 var w = 600;
-var h = 200;
+var h = 600;
 
 // Create container div for styling purposes
 var main = d3.select('body').append('div')
                 .attr('id', 'main')
                 .style('text-align', 'center');
-main.append('p')
+
+var expression = main.append('p')
 
 // Input text box for expression
 var inputDiv = main.append('div')
@@ -24,22 +25,64 @@ var inputBox = inputDiv.append('input')
                     .style('text-align', 'center');
                     //.attr('placeholder', 'expression');
 
+var jsonExpression = '';
 inputBox.on("input", function() {
-    visualize(inputBox.property('value'));
-    // console.log(inputBox.property('value'));
-
+    jsonExpression = JSON.parse(getJSON(inputBox.property('value')));
+    console.log(jsonExpression);
+    //console.log(jsonExpression['agents']);
     // if valid input, then visualize() without requiring 'enter' key to be pressed
     // NOTE: How to implement 'onSumbit' in this format?
+
+    var coloragent = '#40bf80';
+
+    // force directed graph - currently only nodes are implemented
+    const manuallinks = [];
+    const manualnodes = jsonExpression.agents.map(d => Object.create(d));
+
+    const simulation = d3.forceSimulation(manualnodes)
+      .force("link", d3.forceLink(manuallinks))
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(w / 2, h / 2));
+
+     const link = svg.append("g")
+       .attr("stroke", "#999")
+       .attr("stroke-opacity", 0.6)
+     .selectAll("line")
+     .data(manuallinks)
+     .enter().append("line")
+       .attr("stroke-width", d => Math.sqrt(d.value));
+
+       const node = svg.append("g")
+       .attr("stroke", "#fff")
+       .attr("stroke-width", 1.5)
+     .selectAll("circle")
+     .data(manualnodes)
+     .enter().append("circle")
+       .attr("r", 5)
+       .attr("fill", coloragent);
+
+
+       simulation.on("tick", () => {
+     link
+         .attr("x1", d => d.source.x)
+         .attr("y1", d => d.source.y)
+         .attr("x2", d => d.target.x)
+         .attr("y2", d => d.target.y);
+
+     node
+         .attr("cx", d => d.x)
+         .attr("cy", d => d.y);
+   });
+
 });
 
-// Dummy function just to explore the usage of input box value
-// will contain dynamic Visualization
-function visualize(input) {
-    main.select('p').text( () => {
+// Prints expression to expression
+function getJSON(input) {
+    expression.text( () => {
       let chart = tinynlp.parse([...input], pattern, 'start')
       return JSON.stringify(simplify(chart), null, 2)
-    }
-    );
+    });
+    return expression.text();
 };
 
 // Create parent div for svg
@@ -56,40 +99,7 @@ var svg = d3.select("#svgDiv").append("svg")
                 .attr('id', 'svg');
 
 // Default Visualization
-var dummy = ['node', 'interface', 'interface'];
-
-var circles = svg.selectAll('circle')
-                .data(dummy)
-                .enter()
-                .append('circle');
 
 // TODO: Force directed graph based on input received
 //  POTENTIAL INTERMEDIATE STEP: Base graphics for nodes, links, etc to mirror
 //      figure 5 in Kappa manual (create a 'node' etc)
-circles
-    .attr('cx', function(d, i) {
-        if (d == 'node') {
-            return w/2;
-        } else {
-            if (i % 2 == 0) {
-                return w/2 + (13*(i));
-            } else {
-                return w/2 - (13*(i+1));
-            }
-        }
-    })
-    .style('fill', function(d) {
-        if (d == 'node') {
-            return '#bf0040';
-        } else {
-            return '#73008c';
-        }
-    })
-    .attr('cy', h/4)
-    .attr('r', function(d) {
-        if (d == 'node') {
-            return 20;
-        } else {
-            return 10;
-        }
-    });
